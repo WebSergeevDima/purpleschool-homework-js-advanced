8331ba6996a101e9fd50bddf82b886bb0782f228
Сергеев Дмитрий <sergeev@tbm-technology.ru>
2024-06-24 15:16:32 +0300

    Добавил проверку на валидность дайса

--- 2-dice/app.js
+++ 2-dice/app.js
@@ -1,5 +1,10 @@
 'use strict';
 
-const throwDice = (type) => Math.floor(Math.random() * type.split('d')[1] + 1);
+const throwDice = (type) => {
+    const numbers = new Set([4, 6 ,8 ,10, 12, 16, 20]);
+    const number = type.split('d')[1];
+
+    return numbers.has(Number(number)) ? Math.floor(Math.random() * number + 1) : 'Такого Дайса не существует!';
+};
 
 console.log(throwDice('d6'));
