5ffdac813717b162ce5e772d96a2b60a86101114
Сергеев Дмитрий <sergeev@tbm-technology.ru>
2024-07-03 14:50:18 +0300

    Исправлен дублирующий вызов функции

--- 2-dice/app.js
+++ 2-dice/app.js
@@ -4,17 +4,17 @@ const throwDice = (type) => {
     const getRandomNumber = (number) => Math.floor(Math.random() * number + 1);
 
     const dices = {
-        'd4': getRandomNumber(4),
-        'd6': getRandomNumber(6),
-        'd8': getRandomNumber(8),
-        'd10': getRandomNumber(10),
-        'd12': getRandomNumber(12),
-        'd16': getRandomNumber(16),
-        'd20': getRandomNumber(20),
+        'd4': 4,
+        'd6': 6,
+        'd8': 8,
+        'd10': 10,
+        'd12': 12,
+        'd16': 16,
+        'd20': 20,
     }
 
-    if(dices.hasOwnProperty(type)) {
-        return dices[type];
+    if (dices.hasOwnProperty(type)) {
+        return getRandomNumber(dices[type]);
     }
 
     return 'Такого Дайса не существует!';
